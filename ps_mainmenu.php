fa648ec9edae79035693b0554e48aa954347c749
Maxime Biloé <maxime.biloe@prestashop.com>
2016-09-08 15:44:14 +0200

    // Version updated to v1.0.1

--- ps_mainmenu.php
+++ ps_mainmenu.php
@@ -56,7 +56,7 @@ class Ps_MainMenu extends Module implements WidgetInterface
     {
         $this->name = 'ps_mainmenu';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->author = 'PrestaShop';
 
         $this->bootstrap = true;
