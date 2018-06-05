6bc5ddd52ce92b32f6a6a6e296bba09ecf2e6ed1
Pablo Borowicz <eternoendless@gmail.com>
2018-06-05 14:09:19 +0200

    // Update version to v2.1.1

--- ps_mainmenu.php
+++ ps_mainmenu.php
@@ -56,7 +56,7 @@ class Ps_MainMenu extends Module implements WidgetInterface
     {
         $this->name = 'ps_mainmenu';
         $this->tab = 'front_office_features';
-        $this->version = '2.1.0';
+        $this->version = '2.1.1';
         $this->author = 'PrestaShop';
 
         $this->bootstrap = true;
